6d57e80f7d8e09dba8f24edfdea176120f4e859d
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-08-19 18:06:25 +0500

    .env.example and fav-icon added

--- src/cipher.ts
+++ src/cipher.ts
@@ -1,21 +1,32 @@
 import crypto from "crypto";
 
 export default function cipher(plaintext, password) {
-    const algorithm = 'aes-256-gcm';
-    const key = crypto.pbkdf2Sync(password, Buffer.alloc(16), 100000, 32, 'sha256');
-    const iv = Buffer.alloc(16); 
+  const algorithm = "aes-256-gcm";
+  const key = crypto.pbkdf2Sync(
+    (password + process.env.SITE_SECRET),
+    Buffer.alloc(16),
+    100000,
+    32,
+    "sha256",
+  );
 
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const iv = Buffer.alloc(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+    
+  let encryptedData = cipher.update(plaintext, "utf8", "base64");
+  encryptedData += cipher.final("base64");
 
-    let encryptedData = cipher.update(plaintext, 'utf8', 'base64');
-    encryptedData += cipher.final('base64');
+  const authTag = cipher.getAuthTag();
 
-    const authTag = cipher.getAuthTag();
-
-    return reverseStr(`${iv.toString('base64')}:${authTag.toString('base64')}:${encryptedData}`);
+  return reverseStr(
+    `${iv.toString("base64")}:${authTag.toString("base64")}:${encryptedData}`,
+  );
 }
 
-export function reverseStr(str: string)
-{
-    return str.split("").map(val => val).reverse().join("")
+export function reverseStr(str: string) {
+  return str
+    .split("")
+    .map((val) => val)
+    .reverse()
+    .join("");
 }
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,35 @@
-import express  from "express";
+import * as dotenv from 'dotenv';
+const configEnv = () => dotenv.config();
+configEnv();
+
+import express from "express";
 import bodyParser from "body-parser";
 import { readFileSync } from "fs";
 
 import cipher from "./cipher";
 
 const app = express();
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    try {
-        const { description, password } = req.query;
-        if((description as string).length > 0 && (password as string).length > 0) {
-            const fileContent = readFileSync("./public/password.html", "utf-8");
-            res.send(fileContent.replace("%password%", cipher(description, password)));
-            return
-        }
-        throw new Error("Invalid query");
-    } catch(err) {
-        const fileContent = readFileSync("./public/home.html", "utf-8");
-        res.send(fileContent);
+  try {
+    const { text, key } = req.query;
+    if ((text as string).length > 0 && (key as string).length > 0) {
+      const fileContent = readFileSync("./public/password.html", "utf-8");
+      res.send(
+        fileContent.replace("%password%", cipher(text as string, key as string)),
+      );
+      return;
     }
+    throw new Error("Invalid query");
+  } catch (err) {
+    console.log(err)
+    const fileContent = readFileSync("./public/home.html", "utf-8");
+    res.send(fileContent);
+  }
 });
 
 app.listen(8080, () => {
-    console.log("The application is listening on port 8080!");
+  console.log("The application is listening on port 8080!");
 });
-
-
